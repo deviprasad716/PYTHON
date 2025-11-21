65ee0c518451219e553cb12fdb51c08414b35436
deviprasad716 <deviprasadkalmalemath@gmail.com>
2025-11-21 21:12:45 +0530

    Add files via upload

---
+++ areaofcircle.py
@@ -0,0 +1,7 @@
+import math
+
+radius=float(input("Enter the radius of the circle(in m): "))
+
+area=math.pi*pow(radius,2)
+
+print(f"The area of the circle with radius {round(radius,2)}m is: {area}m^2")
